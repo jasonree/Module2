710739b15e08a1bdbd560117eb1e1edf91289f5c
jlee597 <jsn.lee112@gmail.com>
2016-09-15 13:55:42 +1200

    ddd

--- main.ts
+++ main.ts
@@ -1,1 +1,10 @@
 var myAPI = "4b4ed19606331172627ba0a85c0de2dd";
+var goBtn = $("#goButton")[0];
+var input = $("#city")[0];
+
+
+goBtn.addEventListener("click", function () {
+var city = input;
+console.log(city, "Hello, world!");
+});
+
