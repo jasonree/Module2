293a3b0feac74044b342abe50948e421675a4551
jlee597 <jsn.lee112@gmail.com>
2016-09-15 23:56:20 +1200

    final

--- main.js
+++ main.js
@@ -1,12 +1,22 @@
-/// <reference path="typings/index.d.ts"/>
-var myAPI = "4b4ed19606331172627ba0a85c0de2dd";
+var myAPI = "AIzaSyCWy7rPbHfl3O6pPL0Pv0-mGIsGed7_p4A";
 var goBtn = $("#goButton");
 var input = $("#city");
+
+
 $('form').submit(function (event) {
     event.preventDefault();
     if (input.val().length !== 0) {
         var city = input.val();
         console.log(city);
         input.val('');
-    }
-});
+        var cityEdit = city.split(' ').join('%20');
+        var geoLocation = "https://maps.googleapis.com/maps/api/geocode/json?address=" + cityEdit + "&key=" + myAPI;
+        
+        $.ajax({
+         type: 'GET',
+         url: geoLocation,
+         success: function(data) {
+             console.log('success', data)
+         }
+})
+}});
